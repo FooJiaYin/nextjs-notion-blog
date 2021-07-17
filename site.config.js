cc8d15d24c7adcba2f17c2fb9f767514a2cbfb47
FooJiaYin <candy23198@hotmail.com>
2021-07-18 03:26:08 +0800

    Co-edit with 0ven

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
   // rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-  rootNotionPageId: 'e8e35a8e969b4a7b834f8d1c205f2860',
+  rootNotionPageId: 'fdeb349d34e44c9686fa2f532709b568',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Foo Jia Yin\'s Blog',
+  name: 'Foo_Bar()',
   domain: 'foojiayin.github.io',
   author: 'Foo Jia Yin',
 
